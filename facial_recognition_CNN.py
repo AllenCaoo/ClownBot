63d0a3cd6bdedaf0a2aeeb736a561895f50ccfbc
Allen Cao <allencao@berkeley.edu>
2021-06-16 10:50:52 -0700

    Imported facenet modules

---
+++ facial_recognition_CNN.py
@@ -0,0 +1,8 @@
+import math
+import numpy as np
+from facenet_pytorch import MTCNN
+import cv2
+
+# This implementation is slower, but more accurate compared to facial_recognition_haar.py
+# It uses pre-trained CNN's rather than haar cascades
+

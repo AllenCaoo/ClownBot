22cefe4ff8b247fc52e879915a7dca8dd42bf11a
Allen Cao <allencao@berkeley.edu>
2021-06-16 11:46:15 -0700

    Finished implementing CNN facial + nose recognition

--- facial_recognition_CNN.py
+++ facial_recognition_CNN.py
@@ -6,3 +6,30 @@ import cv2
 # This implementation is slower, but more accurate compared to facial_recognition_haar.py
 # It uses pre-trained CNN's rather than haar cascades
 
+# Create face detector
+def run(img_path):
+    mtcnn = MTCNN(keep_all=True)
+    img = cv2.imread('temp3.jpg')
+    rev = img.copy()
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    # Detect face
+    _, _, landmarks = mtcnn.detect(img, landmarks=True)
+
+    # Draw nose
+    if landmarks:
+        for landmark in landmarks:
+            pt1 = np.int_(landmark[0, 0]), np.int_(landmark[0, 1])
+            pt2 = np.int_(landmark[1, 0]), np.int_(landmark[1, 1])
+            pt3 = np.int_(landmark[3, 0]), np.int_(landmark[3, 1])
+            pt4 = np.int_(landmark[4, 0]), np.int_(landmark[4, 1])
+            features_width = math.dist(pt1, pt2)
+            features_height = math.dist(pt2, pt3)
+            nose_size = int(features_width / 3)  # Approximate nose size
+            # Nose is located at landmark[2:3, 0], landmark[2:3, 1]
+            cv2.circle(rev, (int(landmark[2:3, 0]), int(landmark[2:3, 1])),
+                       nose_size, (0, 0, 255), thickness=-1)
+            # DEBUG: cv2.rectangle(frame, pt1, pt4, (0, 0, 255), thickness=1)
+            return rev
+    else:
+        return None

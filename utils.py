1934ddbbd38ca310e675e2af1ea91e40a41d80ef
Allen Cao <allencao@berkeley.edu>
2021-06-19 00:22:23 -0700

    Reformatted utils.py

--- utils.py
+++ utils.py
@@ -1,10 +1,10 @@
 import math
 
-
 circus_tent = ":circus_tent:"
 partying_face = ":partying_face:"
 yes_no_questions = ['is', 'are', 'will', 'wont', "won't", 'do', 'does', "don't",
                     'dont', 'doesnt', "doesn't", "am", "should", "can"]
+
 
 def dist(pt1, pt2):
     x1 = pt1[0]

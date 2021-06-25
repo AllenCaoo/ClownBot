42288d197ef2d1c48d33406bf17edf9d925a27f9
Allen Cao <allencao@berkeley.edu>
2021-06-24 17:09:19 -0700

    Reorganized code

--- facial_recognition_CNN.py
+++ facial_recognition_CNN.py
@@ -23,15 +23,11 @@ def draw_nose(img_path):
         for landmark in landmarks:
             pt1 = np.int_(landmark[0, 0]), np.int_(landmark[0, 1])
             pt2 = np.int_(landmark[1, 0]), np.int_(landmark[1, 1])
-            pt3 = np.int_(landmark[3, 0]), np.int_(landmark[3, 1])
-            pt4 = np.int_(landmark[4, 0]), np.int_(landmark[4, 1])
             features_width = utils.dist(pt1, pt2)
-            features_height = utils.dist(pt2, pt3)
-            nose_size = int(features_width / 3)  # Approximate nose size
-            # Nose is located at landmark[2:3, 0], landmark[2:3, 1]
-            cv2.circle(rev, (int(landmark[2:3, 0]), int(landmark[2:3, 1])),
-                       nose_size, (0, 0, 255), thickness=-1)
-            # DEBUG: cv2.rectangle(frame, pt1, pt4, (0, 0, 255), thickness=1)
+            nose_size = int(features_width / 3)  # Approximate nose radius
+            x = int(landmark[2:3, 0])  # Nose x is located at landmark[2:3, 0]
+            y = int(landmark[2:3, 1])  # Nose y is located at landmark[2:3, 0]
+            cv2.circle(rev, (x, y), nose_size, (0, 0, 255), thickness=-1)
             found = True
         cv2.imwrite('images/recent_out.jpg', rev)
         return found
